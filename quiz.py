ad1f52276baf5f4b5a62def39f82bc0ec7e529e1
veronmmason@gmail.com <veronmmason@gmail.com>
2021-07-06 21:16:13 -0400

    Created the files and base code for the timed quiz app from the Treehouse videos. I will complete the program in the next commit.

---
+++ quiz.py
@@ -0,0 +1,19 @@
+import datetime
+import random
+
+from questions import Add, Multiply
+
+
+class Quiz:
+    questions = []
+    answers = []
+    
+    def __init__(self):
+        pass
+
+    def take_quiz(self):
+        pass
+
+    def ask(self, question):
+        pass
+
